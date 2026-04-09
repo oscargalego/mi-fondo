c38562e8944a27be1074eec471065b9622f8afc0
oscargalego <oscar.galego@gmail.com>
2026-04-09 19:22:01 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,15 @@
-const CACHE = 'vanguard-pwa-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
+const CACHE = 'vanguard-pwa-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
